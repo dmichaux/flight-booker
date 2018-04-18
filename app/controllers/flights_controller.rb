bb7746e55ccd3108d540b9ec64b0380a129aa41f
David Michaux <michaux.dr@gmail.com>
2018-04-18 15:07:29 -0400

    Add index action

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,2 +1,14 @@
 class FlightsController < ApplicationController
+
+	def index
+		@airports = Airport.all
+		if params.has_key?(:from_airport_id)
+			date = "#{params[:date][:day]}/#{params[:date][:month]}/#{params[:date][:year]}"
+			@passenger_count = params[:passenger_count]
+			@flights = Flight.find_available(params[:from_airport_id],
+																			 params[:to_airport_id],
+																			 @passenger_count,
+																			 date)
+		end
+	end
 end
