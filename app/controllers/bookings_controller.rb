6e55976c065fd9002d47149aeeb6a5acb14055de
David Michaux <michaux.dr@gmail.com>
2018-04-18 15:33:38 -0400

    Add Flights#create action with Strong Params method

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,22 @@
 class BookingsController < ApplicationController
 
 	def new
-		
+		@booking = Booking.new(flight_id: params[:selected_flight])
+		(params[:passenger_count].to_i).times { @booking.passengers.build }
+	end
+
+	def create
+		@booking = Booking.new(booking_params)
+		if @booking.save
+			redirect_to @booking
+		end
+	end
+
+	private
+
+	def booking_params
+		params.require(:booking)
+					.permit(:flight_id,
+									passengers_attributes: [:id, :name, :age])
 	end
 end
